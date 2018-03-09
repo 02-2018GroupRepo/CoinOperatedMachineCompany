d0ee55ad49f0d98d702d7405af8f895c424072b4
TobyBlanchard <blanchard.toby@gmail.com>
2018-03-09 16:42:44 -0500

    second commit

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,7 +1,52 @@
-public interface VendingMachine {
+import java.util.ArrayList;
+import java.util.List;
 
-    public void insertCoin();
-    public void addLocation();
-    public void selectItem();
-    public void dispenseItem();
+public class SnackMachine implements VendingMachine {
+
+    private String location;
+    private int numberOfMachines;
+    private int[] shelves = new int[5];
+    private int[] compartments = new int[5];
+    private int[] spaces = new int[10];
+    private List<Products> items = new ArrayList<Products>();
+
+
+
+    public void initializeSnackMachine(Products product){
+
+
+    }
+    public void addItemIdToMachine(Products p){
+        items.add(p);
+    }
+
+
+    @Override
+    public void insertCoin() {
+
+    }
+
+    @Override
+    public void addLocation() {
+
+    }
+
+    @Override
+    public void selectItem() {
+
+    }
+
+    @Override
+    public void dispenseItem() {
+
+    }
+    public void displayItems(){
+        for(Products p : items){
+            System.out.println(p.getId());
+            System.out.println(p.getName());
+            System.out.println(p.getDescription());
+            System.out.println(p.getRetailPrice());
+
+        }
+    }
 }
