6305fb933c042d63d5075bf658848352ce53aeb3
syntel <syntel@PHX440G3-28160M.syntelorg.com>
2018-03-09 13:27:54 -0500

    more code

--- src/com/tawnihollers/SnackMachine.java
+++ src/com/tawnihollers/SnackMachine.java
@@ -1,5 +1,21 @@
 package com.tawnihollers;
 
+import java.util.ArrayList;
+
 public class SnackMachine extends VendingMachines {
 
+    private String id;
+    private String name;
+    private String description;
+    private double wholeSalePrice;
+    private double retailSalePrice;
+
+
+    public void Product(String id, String name, String description, double wholeSalePrice, double retailSalePrice){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.wholeSalePrice = wholeSalePrice;
+        this.retailSalePrice = retailSalePrice;
+    }
 }
