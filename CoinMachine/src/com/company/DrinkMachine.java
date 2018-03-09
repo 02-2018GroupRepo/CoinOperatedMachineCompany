69b52630ddba2d8abcbea5e94bf6ff16fcf78626
Jrod9599 <Jrod9599@gmail.com>
2018-03-09 14:54:17 -0500

    added items to drink machine

--- CoinMachine/src/com/company/DrinkMachine.java
+++ CoinMachine/src/com/company/DrinkMachine.java
@@ -4,24 +4,24 @@ public class DrinkMachine extends Vending_Machine {
     public DrinkMachine(){
         MAX_ITEM_COUNT = 296;
         //todo: add items to inStock map
-        Products pepsi      = new Products(1, "pepsi", "pepsi    ", 0.0, 0.0);
-        Products coke       = new Products(2, "coke", "coke     ", 0.0, 0.0);
-        Products sprite     = new Products(3, "sprite", "sprite   ", 0.0, 0.0);
-        Products fanta      = new Products(4, "fanta", "fanta    ", 0.0, 0.0);
-        Products dr         = new Products(5, "dr", "dr       ", 0.0, 0.0);
-        Products pepper     = new Products(6, "pepper", "pepper   ", 0.0, 0.0);
-        Products mountian   = new Products(7, "mountian", "mountian ", 0.0, 0.0);
-        Products dew        = new Products(8, "dew", "dew      ", 0.0, 0.0);
-        Products brisk      = new Products(9, "brisk", "brisk    ", 0.0, 0.0);
-        Products iceT       = new Products(10, "iceT", "iceT     ", 0.0, 0.0);
-        Products coffee     = new Products(11, "coffee", "coffee   ", 0.0, 0.0);
-        Products budlight   = new Products(12, "budlight", "budlight ", 0.0, 0.0);
-        Products miller     = new Products(13, "miller", "miller   ", 0.0, 0.0);
-        Products amstel     = new Products(14, "amstel", "amstel   ", 0.0, 0.0);
-        Products icehouse   = new Products(15, "icehouse", "icehouse ", 0.0, 0.0);
-        Products budweiser  = new Products(16, "budweiser", "budweiser", 0.0, 0.0);
-        Products guiness    = new Products(17, "guiness", "guiness  ", 0.0, 0.0);
-        Products corona     = new Products(18, "corona", "corona   ", 0.0, 0.0);
+        Products pepsi      = new Products(1, "pepsi", "pepsi    ", 0.10, 0.50);
+        Products coke       = new Products(2, "coke", "coke     ", 0.10, 0.50);
+        Products sprite     = new Products(3, "sprite", "sprite   ", 0.10, 0.50);
+        Products fanta      = new Products(4, "fanta", "fanta    ", 0.10, 0.50);
+        Products dr         = new Products(5, "dr", "dr       ", 0.10, 0.50);
+        Products pepper     = new Products(6, "pepper", "pepper   ", 0.10, 0.50);
+        Products mountian   = new Products(7, "mountian", "mountian ", 0.10, 0.50);
+        Products dew        = new Products(8, "dew", "dew      ", 0.10, 0.50);
+        Products brisk      = new Products(9, "brisk", "brisk    ", 0.10, 0.50);
+        Products iceT       = new Products(10, "iceT", "iceT     ", 0.10, 1.50);
+        Products coffee     = new Products(11, "coffee", "coffee   ", 0.20, 0.0);
+        Products budlight   = new Products(12, "budlight", "budlight ", 2.00, 5.00);
+        Products miller     = new Products(13, "miller", "miller   ", 2.00, 5.00);
+        Products amstel     = new Products(14, "amstel", "amstel   ", 2.00, 5.00);
+        Products icehouse   = new Products(15, "icehouse", "icehouse ", 2.00, 5.00);
+        Products budweiser  = new Products(16, "budweiser", "budweiser", 2.00, 5.00);
+        Products guiness    = new Products(17, "guiness", "guiness  ", 2.00, 5.00);
+        Products corona     = new Products(18, "corona", "corona   ", 2.00, 5.00);
 
         Products water0   = new Products(19,"water0", "water0", 0.50, 1.00);
         Products water1   = new Products(20,"water1", "water1", 0.50, 1.00);
@@ -52,7 +52,7 @@ public class DrinkMachine extends Vending_Machine {
         inStock.put(budweiser,12);
         inStock.put(guiness  ,12);
         inStock.put(corona   ,12);
-        
+
         inStock.put(water0,8);
         inStock.put(water1,8);
         inStock.put(water2,8);
