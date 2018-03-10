f965f63bda226ac99a466e85e326b865d5951a4c
MuteBard <csevere@brandeis.edu>
2018-03-10 08:06:51 -0500

    create SevereCo instance

---
+++ SevereCo.java
@@ -0,0 +1,18 @@
+public class SevereCo{
+
+    //state
+    private String name;
+
+    //constructor
+    public SevereCo(String name){
+        this.name = name;
+        
+    }
+
+    //behavior
+    //none yet
+}
+
+
+
+
