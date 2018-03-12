ed8c6be12cc9e8d4657300697806c8ecb72795bc
Ngonitiri <Ngonitiri@aol.com>
2018-03-12 15:07:30 -0400

    updated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,8 @@
 package com.company;
 
 public class Main {
-public static void main(String[] args){
-    Products name= new Products();
+public static void main(String[] args)
 
-    System.out.println("snacks");
-
-
-    }
 }
+
+
