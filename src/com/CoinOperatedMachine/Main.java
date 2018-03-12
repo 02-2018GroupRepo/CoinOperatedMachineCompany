8b54f8c1977859de37354e4e0b398424687cce32
syntel <syntel@PHX440G3-2815ZB.syntelorg.com>
2018-03-12 06:47:50 -0400

    minor revision to two scanner objects in main

--- src/com/CoinOperatedMachine/Main.java
+++ src/com/CoinOperatedMachine/Main.java
@@ -6,13 +6,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+
         Scanner coinScanner=new Scanner(System.in);
         System.out.print("Please select an item by entering the letter followed by a number: ");
         double coinsEntered =coinScanner.nextDouble();
 
-        Scanner productSelected=new Scanner(System.in);
+
         System.out.println("Select a product by entering a letter followed by a number-->  ");
-        String=coinScanner.nextLine();
+        String productSelected=coinScanner.nextLine();
 
         if(coinsEntered %5!==0) {
             System.out.println("Please enter the correct amount!");
