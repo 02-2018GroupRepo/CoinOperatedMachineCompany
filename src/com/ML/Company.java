877bffa2c7547b05479abc09770f9fa4de3ff7df
Darkblader714 <MarioL714@gmail.com>
2018-03-12 16:41:43 -0400

    updated locations

--- src/com/ML/Company.java
+++ src/com/ML/Company.java
@@ -4,15 +4,32 @@ public class Company {
 
     String companyName;
 
-    Location kennesaw = new Location("Kennesaw", "GA");
-    Location acworth = new Location("Acworth", "GA");
-    Location marietta = new Location("Marietta", "GA");
-    Location collegePark = new Location("College Park", "GA");
-    Location morrow = new Location("Morrow", "GA");
 
-    Location[] locationList = new Location[5];
+    public static void main(String[] args) {
+        Location kennesaw = new Location("Kennesaw", "GA");
+        Location acworth = new Location("Acworth", "GA");
+        Location marietta = new Location("Marietta", "GA");
+        Location collegePark = new Location("College Park", "GA");
+        Location morrow = new Location("Morrow", "GA");
 
-    locationList[0]=kennesaw;
+        Location[] locationList = new Location[5];
+        locationList[0]=kennesaw;
+        locationList[1]=acworth;
+        locationList[2]=marietta;
+        locationList[3]=collegePark;
+        locationList[4]=morrow;
+
+
+
+        for (int i=0;i<locationList.length;i++){
+
+            System.out.println(locationList[i]);
+
+        }
+
+    }
+
+
 
     public Company (String companyName){
 
