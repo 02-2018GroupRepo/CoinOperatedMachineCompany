d91a5ecc9562c869d6243c9563b671bf191fb3b1
syntel <syntel@PGX440G3-2815WS.syntelorg.com>
2018-03-12 15:30:00 -0400

    made changes to location class

--- MachineCompany/src/Location.java
+++ MachineCompany/src/Location.java
@@ -2,25 +2,23 @@ import java.util.Map;
 public class Location {
     private String location;
     VendingMachine vendingMachine;
-    private String companyName;
+
 
     Map<Integer,VendingMachine> machines;//Integer is for codes for each machine
 
     public Location(){}
 
-    public Location(String location,Map<Integer,VendingMachine> machines,String companyName){
+    public Location(String location,Map<Integer,VendingMachine> machines){
         this.location = location;
         this.machines = machines;
-        this.companyName = companyName;
+
     }
 
     public String getLocation() {
         return location;
     }
 
-    public String getCompanyName() {
-        return companyName;
-    }
+
 
     public void setMachines(VendingMachine machine, int code){
         this.vendingMachine = machine;
