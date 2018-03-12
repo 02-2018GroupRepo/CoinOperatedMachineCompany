5ed8c74d1cb90058e5c9ee9bddbf3046c068b382
Ngonitiri <Ngonitiri@aol.com>
2018-03-12 14:58:42 -0400

    Update

--- src/com/company/Products.java
+++ src/com/company/Products.java
@@ -2,25 +2,58 @@ package com.company;
 
 public class Products {
 
+    private String uuid;
     private String name;
     private String description;
-    private String whloesaleprice;
-    private String retailprice;
-    private String Snacks;
-    private String candy;
-    private String Drinks;{
-       this.Snacks = "almond";
-       this.Snacks = "pretzel";
-       this.Snacks = "cookies";
-       this.Snacks = ("...");
-       this.candy = "chocolate";
-        this.candy = "gum";
-        this.candy = "toffees";
-        this.candy = "...";
-        this.Drinks ="water";
-        this.Drinks ="beverage";
+    private double wholesalePrice;
+    private double retailPrice;
 
+    public Products(String uuid, String name, String description, double wholesalePrice, double retailPrice) {
+        this.uuid = uuid;
+        this.name = name;
+        this.description = description;
+        this.wholesalePrice = wholesalePrice;
+        this.retailPrice = retailPrice;
+    }
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getWholesalePrice() {
+        return wholesalePrice;
+    }
+
+    public void setWholesalePrice(double wholesalePrice) {
+        this.wholesalePrice = wholesalePrice;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice = retailPrice;
     }
 
 
