61259b7c6dc521d033d60acd4fb536995b28ef4a
syntel <syntel@PGX440G3-2815WS.syntelorg.com>
2018-03-13 06:25:26 -0400

    refactored snackMachine

--- MachineCompany/src/SnackMachine.java
+++ MachineCompany/src/SnackMachine.java
@@ -1,25 +1,20 @@
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 public class SnackMachine extends VendingMachine{
     ///need to create an ArrayList of shelves
+    ///need to create method to disp[lay shelves
    private ArrayList<Shelf> shelves;
-
-    private double total;
-
     public final static String machineType = "SnackMachine";
     public  final int spaceSize = 10;
-    public  final int compartMentSize = 5;
+
 
 
     public SnackMachine(){
         shelves = new ArrayList<Shelf>();
 
-        total = 0;
-
     }
 
-
+/*
     public double purchaseItemAndReturnChange(int shelfNumber,int compartMentNumber,Product product){
         Shelf  s1 =  shelves.get(shelfNumber);
         LinkedHashMap<Integer,ArrayList<Product>> shelf =  s1.getShelf();
@@ -27,28 +22,37 @@ public class SnackMachine extends VendingMachine{
         if(getTotal() >= product.getRetail_price()){
             shelf.get(compartMentNumber).remove(product);
             double change = getTotal() - product.getRetail_price();
+            System.out.println("Your total is :"+getTotal());
             System.out.println("Your change is :"+change);
-            total = getTotalAmtAvailableInMachine();
-           total-=change;
+            //total = getTotalAmtAvailableInMachine();
+           //total-=change;
+            balanceOfToTotalAmtAvailableInMachine(change);
            return  change;
 
         }else{
             double amtLeft = product.getRetail_price() - getTotal();
             System.out.println("Insufficient amount please add more coins ");
-            System.out.println("You have "+amtLeft);
+            System.out.println("You have "+amtLeft+" to pay");
             return  getTotal();
         }
     }
+*/
 
-    public double getTotalAmtInMachine(){
-        System.out.println("amount available in machine is :"+total);
-        return total;
-    }
 
+
+  /*
     public ArrayList<Shelf> getShelves() {
         return shelves;
     }
+    */
 
+    public ArrayList<Shelf> createShelves(){
+        for(int i = 0; i < 5;i++){
+            Shelf newShelf = new Shelf(spaceSize);
+            shelves.add(newShelf);
+        }
+        return shelves;
+    }
 
 
 
