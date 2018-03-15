2e47c485ad9a1f652e2a892976e4b91a4cb466f2
syntel <nadario.seays@gmail.com>
2018-03-15 12:30:05 -0400

    adds more code

--- Food4ThoughCoV2/src/com/company/Product.java
+++ Food4ThoughCoV2/src/com/company/Product.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Queue;
+
 public class Product {
 
     private String name;
@@ -7,12 +9,45 @@ public class Product {
     private String uuid;
     private double wholeSalePrice;
     private double retailPrice;
+    private Queue<String> productsQueue;
 
-    public Product(String uuid, String name, String description, double wholeSalePrice, double retailPrice) {
+    public Product(String uuid, String name, String description, double wholeSalePrice, double retailPrice, int qtyOfQueue) {
         this.name = name;
         this.description = description;
         this.uuid = uuid;
         this.wholeSalePrice = wholeSalePrice;
         this.retailPrice = retailPrice;
+        this.makeQueue(qtyOfQueue);
+        
+    }
+
+    public void makeQueue(int qtyOfQueue){
+        for(int i = 0; i < qtyOfQueue; i++){
+            productsQueue.add(uuid);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public double getWholeSalePrice() {
+        return wholeSalePrice;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public Queue<String> getProductsQueue() {
+        return productsQueue;
     }
 }
